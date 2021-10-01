894d2a9840c9c5d54b71da6364a5aad275e803ad
Johnny Hacking <jhacking@bhico.com>
2021-10-01 05:31:31 -0600

    Initial Commit

---
+++ learningiOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learningiOS15
+//
+//  Created by Johnny Hacking on 10/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learningiOS15/learningiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  learningiOS15App.swift
+//  learningiOS15
+//
+//  Created by Johnny Hacking on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct learningiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
